232b56e920443d33cfd8ff15ce9aecf2aacec10e
juanmnl <juanmnlcornejo@gmail.com>
2016-05-25 12:28:00 -0500

    Add templating to build w/ @sauljabin

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,37 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
+var handlebars = require('gulp-handlebars');
+var hb = require('gulp-hb');
 var sass = require('gulp-sass');
 
 // Styles
 gulp.task('styles', function(){
-  return gulp.src('src/styles/main.scss')
+    return gulp.src('./src/styles/main.scss')
     .pipe(sass())
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('./dist'))
     .pipe(browserSync.stream());
 });
 
-// HTML
-gulp.task('html', function(){
-  return gulp.src('src/index.html')
-    .pipe(gulp.dest('dist'))
-    .pipe(browserSync.stream());
+// Templates
+gulp.task('hbs', function() {
+    gulp.src('./src/index.html')
+    .pipe(hb({
+        data: './src/data/*.{js,json}',
+        helpers: './src/helpers/*.js',
+        partials: './src/templates/*.hbs'
+    }))
+    .pipe(gulp.dest('./dist/'));
 });
 
 // Static server
-gulp.task('serve', ['html', 'styles'], function() {
+gulp.task('serve', ['styles', 'hbs'], function() {
     browserSync.init({
         server: {
             baseDir: "./dist"
         }
     });
-    gulp.watch('src/*.html', ['html']).on('change', browserSync.reload);
-    gulp.watch('src/styles/*.scss', ['styles']).on('change', browserSync.reload);
+    gulp.watch('./src/templates/*.hbs', ['hbs']).on('change', browserSync.reload);
+    gulp.watch('./src/styles/*.scss', ['styles']).on('change', browserSync.reload);
 });
 
 gulp.task('default', ['serve']);
