85827e1f6a75ad05f736d8255e2da9314770e048
Saúl Piña <sauljabin@gmail.com>
2016-05-25 14:58:48 -0500

    Add gulp-imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -3,35 +3,46 @@ var browserSync = require('browser-sync').create();
 var handlebars = require('gulp-handlebars');
 var hb = require('gulp-hb');
 var sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
 
 // Styles
-gulp.task('styles', function(){
-    return gulp.src('./src/styles/main.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./dist'))
-    .pipe(browserSync.stream());
+gulp.task('styles', function() {
+  return gulp.src('./src/styles/main.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('./dist'))
+  .pipe(browserSync.stream());
 });
 
 // Templates
 gulp.task('hbs', function() {
-    gulp.src('./src/index.html')
-    .pipe(hb({
-        data: './src/data/*.{js,json}',
-        helpers: './src/helpers/*.js',
-        partials: './src/templates/*.hbs'
-    }))
-    .pipe(gulp.dest('./dist/'));
+  gulp.src('./src/index.html')
+  .pipe(hb({
+    data: './src/data/*.{js,json}',
+    helpers: './src/helpers/*.js',
+    partials: './src/templates/*.hbs'
+  }))
+  .pipe(gulp.dest('./dist'))
+  .pipe(browserSync.stream());
+});
+
+// Images
+gulp.task('images', function() {
+  gulp.src('./src/assets/images/*')
+  .pipe(imagemin())
+  .pipe(gulp.dest('./dist/assets/images'))
+  .pipe(browserSync.stream());
 });
 
 // Static server
-gulp.task('serve', ['styles', 'hbs'], function() {
-    browserSync.init({
-        server: {
-            baseDir: "./dist"
-        }
-    });
-    gulp.watch('./src/templates/*.hbs', ['hbs']).on('change', browserSync.reload);
-    gulp.watch('./src/styles/*.scss', ['styles']).on('change', browserSync.reload);
+gulp.task('serve', ['styles', 'hbs', 'images'], function() {
+  browserSync.init({
+    server: {
+      baseDir: "./dist"
+    }
+  });
+  gulp.watch('./src/templates/*.hbs', ['hbs']).on('change', browserSync.reload);
+  gulp.watch('./src/styles/*.scss', ['styles']).on('change', browserSync.reload);
+  gulp.watch('./src/assets/images/*', ['images']).on('change', browserSync.reload);
 });
 
 gulp.task('default', ['serve']);
