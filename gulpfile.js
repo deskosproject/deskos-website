11587ad822b54a4a0a7ead50bc860459cd6ec398
Saúl Piña <sauljabin@gmail.com>
2016-05-25 17:52:50 -0500

    Merge, gulpfile updated w/ @juanmnl

--- gulpfile.js
+++ gulpfile.js
@@ -11,18 +11,25 @@ gulp.task('html', function() {
   .pipe(browserSync.stream());
 })
 
+// Fonts
+gulp.task('fonts', function() {
+  return gulp.src(['./src/assets/fonts/*','./node_modules/font-awesome/fonts/*'])
+  .pipe(gulp.dest('./dist/assets/fonts'))
+  .pipe(browserSync.stream());
+})
+
 // Scripts
 gulp.task('scripts', function() {
-  return gulp.src('./src/scripts/*.js')
+  return gulp.src(['./src/scripts/*.js','./node_modules/jquery/dist/jquery.min.js','./node_modules/jquery-smooth-scroll/jquery.smooth-scroll.min.js'])
   .pipe(gulp.dest('./dist'))
   .pipe(browserSync.stream());
 })
 
 // Styles
 gulp.task('styles', function() {
-  return gulp.src('./src/styles/main.scss')
+  return gulp.src(['./src/styles/main.scss','./node_modules/bootstrap/dist/css/bootstrap.min.css','./node_modules/font-awesome/css/font-awesome.min.css'])
   .pipe(sass())
-  .pipe(gulp.dest('./dist'))
+  .pipe(gulp.dest('./dist/assets/styles'))
   .pipe(browserSync.stream());
 });
 
@@ -47,13 +54,14 @@ gulp.task('images', function() {
 });
 
 // Static server
-gulp.task('serve', ['html', 'scripts', 'styles', 'hbs', 'images'], function() {
+gulp.task('serve', ['html', 'fonts', 'scripts', 'styles', 'hbs', 'images'], function() {
   browserSync.init({
     server: {
       baseDir: "./dist"
     }
   });
   gulp.watch('./src/*.html', ['html']).on('change', browserSync.reload);
+  gulp.watch('./src/assets/fonts/*', ['fonts']).on('change', browserSync.reload);
   gulp.watch('./src/styles/*.scss', ['styles']).on('change', browserSync.reload);
   gulp.watch('./src/scripts/*.js', ['scripts']).on('change', browserSync.reload);
   gulp.watch('./src/templates/*.hbs', ['hbs']).on('change', browserSync.reload);
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,3 @@
 $(function() {
-  $('a.page-scroll').click(function(event) {
-    var anchor = $(this);
-    $('html, body').animate({
-      scrollTop: $(anchor.attr('href')).offset().top
-    }, 1500, 'easeInOutExpo');
-    event.preventDefault();
-  });
+  $('a.page-scroll').smoothScroll({speed:800});
 });
